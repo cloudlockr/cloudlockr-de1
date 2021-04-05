7e7ac96aa7fc6c190eca3ee83902af0992efd4d5
Over-TheRainbow <54884340+Over-TheRainbow@users.noreply.github.com>
2021-04-04 18:53:49 -0700

    Some quick functions for saving a password

---
+++ CPEN391FW/source/verificationService.c
@@ -0,0 +1,63 @@
+/*
+ * verificationService.c
+ *
+ *  Created on: Apr 4, 2021
+ *      Author: Andrew C
+ */
+
+/*
+ * Description
+ * Functions for verification.
+ */
+
+/*------------------- Constants Define -------------------*/
+#define MASTER_PW_ADDR 0x0300_0000
+
+/*------------------- Type Define --------------------------------------------*/
+/*------------------- Local Data ---------------------------------------------*/
+/*------------------- Local Function Prototype -------------------------------*/
+/*------------------- Local Function -----------------------------------------*/
+/*------------------- API Function -------------------------------------------*/
+
+/**************************************************************************
+** Takes a 32 character password, and copies it to the master password
+** address.
+**
+** Parameters:
+**     password      char pointer to the password string
+**
+***************************************************************************/
+void setPassword( char *password )
+{
+	for ( unsigned char i = 0; i < 32; i++ )
+	{
+		*(MASTER_PW_ADDR + i) = *(password + i);
+	}
+}
+
+
+/**************************************************************************
+** Copies the master password to the password string.
+**
+** Parameters:
+**     password      char pointer to the password string
+**
+***************************************************************************/
+void getPassword( char *password )
+{
+	for ( unsigned char i = 0; i < 32; i++ )
+	{
+		*(password + i) = *(MASTER_PW_ADDR + i);
+	}
+}
+
+
+/**************************************************************************
+** thingy
+**
+** Parameters:
+**     password      int *me
+**     hex           char *you
+**
+***************************************************************************/
+//bool verify(password, hex)
---
+++ CPEN391FW/source/verificationService.h
@@ -0,0 +1,18 @@
+/*
+ * verificationService.h
+ *
+ *  Created on: Apr 4, 2021
+ *      Author: David
+ */
+
+#ifndef VERIFICATIONSERVICE_H_
+#define VERIFICATIONSERVICE_H_
+
+
+
+#endif /* VERIFICATIONSERVICE_H_ */
+
+/*------------------- Type Define -------------------*/
+/*------------------- Function Prototype -------------------*/
+void setPassword( char *password );
+void getPassword( char *password );
