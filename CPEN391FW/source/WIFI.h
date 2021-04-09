087672122319dff3bb555de9bfe6b3077251294f
Ihsan Olawale <ihsanolawale@outlook.com>
2021-04-09 09:43:00 -0700

    Make esp functions static and fix initiate_tcp

--- CPEN391FW/source/WIFI.h
+++ CPEN391FW/source/WIFI.h
@@ -16,14 +16,10 @@
 
 
 char* uploadData(char* email, char* fileId, char* packetNumber, char* totalPackets, char* fileData);
-char* get_wifi_networks( void ); // Look at AT+CWLAP
-//int set_wifi_config(char* networkName, char* networkPassword);
+//char* get_wifi_networks( void ); // Look at AT+CWLAP
+int set_wifi_config(char* networkName, char* networkPassword);
 char* getFileMetadata(char* fileId);
 char* getBlob(char* fileId, char* blobNumber);
-
-bool esp8266_send_command(const char *cmd);
-bool esp8266_send_data( const char *data, int length, char *buffer);
-
 
 
 #endif /* WIFI_H_ */
--- CPEN391FW/source/wifiService.c
+++ CPEN391FW/source/wifiService.c
@@ -6,86 +6,27 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-char* uploadData(char* email, char* fileId, char* packetNumber, char* totalPackets, char* fileData) {
-	return NULL;
+static void esp8266_dump_rx( void )
+{
+    char ch;
+
+    while ( 1 )
+    {
+        if ( UART_TestForReceivedData( UART_ePORT_WIFI ) )
+	    {
+	        ch = UART_getchar( UART_ePORT_WIFI );
+            printf( "dump Rx: %c\n", ch );
+	    }
+        else
+        {
+            break;
+        }
+    }
+
+    UART_Flush( UART_ePORT_WIFI );
 }
 
-//char* get_wifi_networks( void ) {
-//	bool success = true;
-//	char buffer[1000];
-//	char response[2000];
-//
-//    esp8266_send_command("AT+CWMODE_CUR=1");
-//    HPS_usleep( 3000000 );
-//    esp8266_send_command("AT+CWLAPOPT=1,0x2");
-//    HPS_usleep( 3000000 );
-////    HPS_usleep(1000000);
-//    success = esp8266_send_command("AT+CWLAP");
-//	if (success) {
-//		// +CWLAP:<ecn>, <ssid>, <rssi>, <mac>, <ch>, <freq offset>, <freq calibration>
-//		// <ecn>, <rssi>, <mac>, <ch>, <freq offset>, <freq calibration>: not used
-//		// <ssid>: name of WiFi network
-//
-//		// sprintf(response, "{\"status\":1,\"networks\":\"");
-//	}
-//	return NULL;
-//}
-
-int set_wifi_config(char* networkName, char* networkPassword) {
-	char cmd[1000];
-    esp8266_send_command("AT+CWMODE_CUR=1");
-    esp8266_send_command("AT+CWLAPOPT=1,0x2");
-    sprintf(cmd, "AT+CWJAP_CUR=\"%s\",\"%s\"", networkName, networkPassword);
-	return esp8266_send_command(cmd);
-}
-
-static bool initiate_tcp(char *domain) {
-	bool success;
-    char cmd_buffer[100];
-	sprintf(cmd_buffer, "AT+CIPSTART=\"TCP\",\"%s\",80", domain); // start TCP connection at domain with port 80
-	success = esp8266_send_command(cmd_buffer);
-	return success;
-}
-
-char* getFileMetadata(char* fileId) {
-    char cmd_buffer[100];
-    char request[1000];
-    char response[1000];
-    sprintf(request, "GET /file/%s HTTP/1.1\r\nHost: cloudlockr.herokuapp.com\r\nUser-Agent: terasic-rfs\r\n\r\n", fileId);
-	if (initiate_tcp("http://cloudlockr.herokuapp.com/")) {
-        sprintf(cmd_buffer, "AT+CIPSEND=%d", strlen(request)); // specify length of GET command
-		if (esp8266_send_command(cmd_buffer)) {
-			if (esp8266_send_data( request, strlen(request), response)) {
-				// do something with response
-				return "{\"status\": 1}";
-			}
-			return "{\"status\": 0}";
-		}
-		return NULL;
-	}
-	return NULL;
-}
-
-char* getBlob(char* fileId, char* blobNumber) {
-    char cmd_buffer[100];
-    char request[1000];
-    char response[1000];
-    sprintf(request, "GET /file/%s/%s HTTP/1.1\r\nHost: cloudlockr.herokuapp.com\r\nUser-Agent: terasic-rfs\r\n\r\n", fileId, blobNumber);
-	if (initiate_tcp("https://cloudlockr.herokuapp.com/")) {
-        sprintf(cmd_buffer, "AT+CIPSEND=%d", strlen(request)); // specify length of GET command
-		if (esp8266_send_command(cmd_buffer)) {
-			if (esp8266_send_data( request, strlen(request), response )) {
-				// do something with response
-				return "{\"status\": 1}";
-			}
-			return "{\"status\": 0}";
-		}
-		return NULL;
-	}
-	return NULL;
-}
-
-bool esp8266_send_command(const char *cmd)
+static bool esp8266_send_command(const char *cmd)
 {
     int length = 0;
     char buffer[1000];
@@ -125,7 +66,7 @@ bool esp8266_send_command(const char *cmd)
     return false;
 }
 
-bool esp8266_send_data( const char *data, int length, char *buffer)
+static bool esp8266_send_data( const char *data, int length, char *buffer)
 {
     int count = 0;
 
@@ -156,3 +97,83 @@ bool esp8266_send_data( const char *data, int length, char *buffer)
 
     return false;
 }
+
+char* uploadData(char* email, char* fileId, char* packetNumber, char* totalPackets, char* fileData) {
+	return NULL;
+}
+
+static bool initiate_tcp(char *domain) {
+	bool success;
+    char cmd_buffer[100];
+	sprintf(cmd_buffer, "AT+CIPSTART=\"TCP\",\"%s\",80", domain); // start TCP connection at domain with port 80
+	success = esp8266_send_command(cmd_buffer);
+	return success;
+}
+
+//char* get_wifi_networks( void ) {
+//	bool success = true;
+//	char buffer[1000];
+//	char response[2000];
+//
+//    esp8266_send_command("AT+CWMODE_CUR=1");
+//    HPS_usleep( 3000000 );
+//    esp8266_send_command("AT+CWLAPOPT=1,0x2");
+//    HPS_usleep( 3000000 );
+////    HPS_usleep(1000000);
+//    success = esp8266_send_command("AT+CWLAP");
+//	if (success) {
+//		// +CWLAP:<ecn>, <ssid>, <rssi>, <mac>, <ch>, <freq offset>, <freq calibration>
+//		// <ecn>, <rssi>, <mac>, <ch>, <freq offset>, <freq calibration>: not used
+//		// <ssid>: name of WiFi network
+//
+//		// sprintf(response, "{\"status\":1,\"networks\":\"");
+//	}
+//	return NULL;
+//}
+
+int set_wifi_config(char* networkName, char* networkPassword) {
+	char cmd[1000];
+    esp8266_send_command("AT+CWMODE_CUR=1");
+    esp8266_send_command("AT+CWLAPOPT=1,0x2");
+    sprintf(cmd, "AT+CWJAP_CUR=\"%s\",\"%s\"", networkName, networkPassword);
+	return esp8266_send_command(cmd);
+}
+
+char* getFileMetadata(char* fileId) {
+    char cmd_buffer[100];
+    char request[1000];
+    char response[1000];
+    sprintf(request, "GET /file/%s HTTP/1.1\r\nHost: cloudlockr.herokuapp.com\r\nUser-Agent: terasic-rfs\r\n\r\n", fileId);
+	if (initiate_tcp("cloudlockr.herokuapp.com")) {
+        sprintf(cmd_buffer, "AT+CIPSEND=%d", strlen(request)); // specify length of GET command
+		if (esp8266_send_command(cmd_buffer)) {
+			if (esp8266_send_data( request, strlen(request), response)) {
+				// do something with response
+				return "{\"status\": 1}";
+			}
+			return "{\"status\": 0}";
+		}
+		return NULL;
+	}
+	return NULL;
+}
+
+char* getBlob(char* fileId, char* blobNumber) {
+    char cmd_buffer[100];
+    char request[1000];
+    char response[1000];
+    sprintf(request, "GET /file/%s/%s HTTP/1.1\r\nHost: cloudlockr.herokuapp.com\r\nUser-Agent: terasic-rfs\r\n\r\n", fileId, blobNumber);
+	if (initiate_tcp("https://cloudlockr.herokuapp.com/")) {
+        sprintf(cmd_buffer, "AT+CIPSEND=%d", strlen(request)); // specify length of GET command
+		if (esp8266_send_command(cmd_buffer)) {
+			if (esp8266_send_data( request, strlen(request), response )) {
+				// do something with response
+				return "{\"status\": 1}";
+			}
+			return "{\"status\": 0}";
+		}
+		return NULL;
+	}
+	return NULL;
+}
+
