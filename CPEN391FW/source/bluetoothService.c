da6dfd07002863ecb5f28c8db0e4c281fe3ef79e
zfrantzen <43505724+zfrantzen@users.noreply.github.com>
2021-04-11 10:57:41 -0700

    integrated upgraded bluetooth with refactor branch. Cleaned up mocking code.

--- CPEN391FW/source/bluetoothService.c
+++ CPEN391FW/source/bluetoothService.c
@@ -8,147 +8,143 @@
 /*
  * Description:
  * This module implements bluetooth related functionalities.
+ *
+ * Author:
+ * zfrantzen
  */
 
 #include <stdio.h>
 #include <string.h>
 
-#include "bluetoothService.h"
-#include "constants.h"
 #include "TypeDef.h"
 #include "hpsService.h"
 #include "UART.h"
 #include "JsonParser.h"
 
-static int bluetooth_count = 0;
-static int fragment_count = 0;
+#define BUFFER_SIZE 500500 // 1 mb of data (assuming 2 bytes per char) + 500 bytes of extra data allowance
+#define TIMEOUT_ITER 10000000 // Large number to represent the max number of iterations with no data arriving (~50 sec)
+static int  bluetooth_count = 0;
 static char bluetooth_data[BUFFER_SIZE];
-static char last_two_data[2] = "";
 
-typedef enum
-{
-	STATE_PARSE_MSG = 0,
-	STATE_MSG_VALUE, // may not be needed
-	STATE_JSON_PARSE,
-} STATE_T;
+/* Mocking settings */
+#define MOCK_BLUETOOTH 0
+static int mock_idx = 0;
 
 /*
  * Sends a JSON message to the phone over bluetooth. Assumes that the string has already been
  * properly formatted as a valid JSON object and has special characters like quotations backslashed.
- *
- * The main purpose of this function is to fragment the messages and send them in the
- * expected format.
+ * Must have "\v\n" at the end of the data string.
  */
-void bluetooth_send_message(char *data)
+void bluetooth_send_message( char* data )
 {
-	// TODO: need to still implement (and convert bluetooth_process() to use it)
-	(void)data;
+#if !MOCK_BLUETOOTH
+	UART_puts( UART_ePORT_BLUETOOTH, data );
+#endif
 }
 
 /*
  * Special communication method for sending a specific status code
  * (most communications only require this)
  */
-void bluetooth_send_status(int status)
+void bluetooth_send_status( int status )
 {
+#if !MOCK_BLUETOOTH
 	// Format message and send
 	char res_buffer[18];
 	snprintf(res_buffer, sizeof(res_buffer), "{\"status\":%d}\v\n", status);
-	UART_puts(UART_ePORT_BLUETOOTH, res_buffer);
-}
-
-/*
- * Returns a parsed json token after all data has been processed. NULL otherwise.
- */
-char *bluetooth_process(char ch)
-{
-	bool full_message_received = false;
-	bool fragment_received = false;
-	int status = 1;
-
-	// Process passed char (if buffer space is available)
-	if (bluetooth_count < BUFFER_SIZE)
-	{
-		// Save only valid chars to the bluetooth_data buffer
-		if (ch != '\n' && ch != '\r' && ch != '\v')
-		{
-			bluetooth_data[bluetooth_count] = ch;
-			bluetooth_count++;
-			fragment_count++;
-		}
-
-		last_two_data[0] = last_two_data[1];
-		last_two_data[1] = ch;
-
-		// Check message end conditions
-		if (last_two_data[0] == '\v' && last_two_data[1] == '\n')
-		{
-			full_message_received = true;
-		}
-		else if (last_two_data[0] == '\r' && last_two_data[1] == '\n')
-		{
-			fragment_received = true;
-		}
-	}
-	else if (bluetooth_count >= BUFFER_SIZE)
-	{
-		// Mark a fragmentation error
-		full_message_received = true;
-		status = 3;
-
-		// Move the buffer cursor back to "erase" collected fragment data
-		bluetooth_count -= fragment_count;
-	}
-
-	// Respond to received full messages (either full or a fragment)
-	if (fragment_received)
-	{
-		fragment_count = 0;
-
-		if (status == 1)
-			// Assign the correct OK status
-			status = 2;
-
-		bluetooth_send_status(status);
-	}
-	else if (full_message_received)
-	{
-		// NULL terminate the buffer
-		bluetooth_data[bluetooth_count] = 0;
-
-		// Acknowledge the fragment
-		bluetooth_send_status(2);
-
-		// Reset state
-		bluetooth_count = 0;
-		fragment_count = 0;
-
-		printf("msg received:\n");
-		printf(bluetooth_data);
-		printf("\n");
-
-		return bluetooth_data;
-	}
-
-	return NULL;
+	UART_puts( UART_ePORT_BLUETOOTH, res_buffer );
+#endif
 }
 
 /*
  * Waits for an entire bluetooth message to be received and processed
  */
-char *bluetooth_wait_for_data(void)
+char* bluetooth_wait_for_data( void )
 {
+#if MOCK_BLUETOOTH
+	return mock_bluetooth_wait_for_data();
+#else
+
+	// Reset state
+	bluetooth_count = 0;
+	int i = 0;
+	char c = ' ';
+	
+	// Wait for initial data to arrive
 	while (1)
 	{
-		if (UART_TestForReceivedData(UART_ePORT_BLUETOOTH))
+		if ( UART_TestForReceivedData( UART_ePORT_BLUETOOTH ) )
 		{
-			char ch = (char)UART_getchar(UART_ePORT_BLUETOOTH);
-			char *full_json_string = bluetooth_process(ch);
-
-			if (full_json_string != NULL)
-			{
-				return full_json_string;
-			}
+			c = (char)UART_getchar( UART_ePORT_BLUETOOTH );
+			bluetooth_data[bluetooth_count] = c;
+			bluetooth_count++;
+			break;
 		}
 	}
+
+	// Process all subsequent data (timing out if too much waiting elapses)
+	while (i < TIMEOUT_ITER && c != '\n')
+	{
+		if ( UART_TestForReceivedData( UART_ePORT_BLUETOOTH ) )
+		{
+			c = (char)UART_getchar( UART_ePORT_BLUETOOTH );
+			bluetooth_data[bluetooth_count] = c;
+
+			bluetooth_count++;
+			i = 0;
+
+			continue;
+		}
+
+		i++;
+	}
+	
+	// Return NULL if timeout occurs
+	if (c != '\n')
+	{
+		return NULL;
+	}
+
+	// Otherwise, acknowledge the fragment and null terminate (and truncate line endings)
+	bluetooth_send_status(2);
+	bluetooth_data[bluetooth_count - 2] = 0;
+
+	return bluetooth_data;
+#endif
+}
+
+char* mock_bluetooth_wait_for_data( void )
+{
+	char *json_str = NULL;
+	if (mock_idx == 0)
+	{
+		json_str = "{\"type\":7,\"password\":\"1234567890abc\"}";
+		mock_idx++;
+	}
+	else if (mock_idx == 1)
+	{
+		json_str = "{\"type\":1}";
+		mock_idx++;
+	}
+	else if (mock_idx == 2)
+	{
+		json_str = "{\"type\":2,\"password\":\"1234567890abc\",\"hex\":\"ABCDEF\"}";
+		mock_idx++;
+	}
+	else if (mock_idx == 3)
+	{
+		json_str = "{\"type\":3,\"fileId\":\"123\",\"packetNumber\":1,\"totalPackets\":3,\"location\":\"37.422|-122.084|5.285\",\"fileData\":\"1234567890abcdeffedcba0987654321\"}";
+		mock_idx++;
+	}
+	else if (mock_idx == 4)
+	{
+		json_str = "{\"type\":4,\"localEncryptionComponent\":\"0102ABCD\",\"fileId\":\"123\",\"location\":\"37.422|-122.084|5.285\"}";
+		mock_idx++;
+	}
+	else
+	{
+		mock_idx = 0;
+	}
+
+	return json_str;
 }
