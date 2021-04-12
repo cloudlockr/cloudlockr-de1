95a5f121741af677da3fb72975592c2ddc65a78e
Andrew <shatterstorm34@gmail.com>
2021-04-12 12:57:05 -0700

    deleted more files

--- CPEN391_Computer (Verilog) UART - For 391 Students/ColourPallette_2PortRam_inst.vhd
+++
@@ -1,8 +0,0 @@
-		data	 => data_sig,
-		rdaddress	 => rdaddress_sig,
-		rdclock	 => rdclock_sig,
-		wraddress	 => wraddress_sig,
-		wrclock	 => wrclock_sig,
-		wren	 => wren_sig,
-		q	 => q_sig
-	);
